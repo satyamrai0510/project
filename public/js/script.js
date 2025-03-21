e557225d94d5c56130a14a8fc01a97f501cb6779
Satyam Rai <satyamrai0510@gmail.com>
2025-03-21 21:35:44 +0530

    Update script.js

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-// Get the name and age from the URL query parameters
-const urlParams = new URLSearchParams(window.location.search);
-const name = urlParams.get('name');
-const age = urlParams.get('age');
-
-// Display the name and age on the page
-document.getElementById('name').textContent = name;
-document.getElementById('age').textContent = age;
